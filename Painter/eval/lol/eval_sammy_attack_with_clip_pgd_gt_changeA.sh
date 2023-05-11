b780e72871d8270d54f847277dacaa1bef90bc50
sammy <920375887@qq.com>
2023-05-11 22:22:28 +0800

    add LOL attack exp

--- Painter/eval/lol/eval_sammy_attack_with_clip_pgd_gt_changeA.sh
+++ Painter/eval/lol/eval_sammy_attack_with_clip_pgd_gt_changeA.sh
@@ -23,12 +23,12 @@ STEP=10
 #### PGD10步 2mins
 
 TASK=lol_enhance
-EXP=attack_baseline
+EXP=attack_baseline_changeA
 DST_DIR="/hhd3/ld/painter_output/${TASK}/${EXP}/${ATTACK}${STEP}_${EPSILON}/${ATTACK_ID}/output/"
 SAVE_DATA_PATH="/hhd3/ld/painter_output/${TASK}/${EXP}/${ATTACK}${STEP}_${EPSILON}/${ATTACK_ID}/save_data/"
 
 
-CUDA_VISIBLE_DEVICES=3 python eval/lol/painter_inference_lol_attack_gt_baseline.py \
+CUDA_VISIBLE_DEVICES=3 python eval/lol/painter_inference_lol_attack_gt_baseline_changeA.py \
     --ckpt_path /hhd3/ld/checkpoint/ckpt_Painter/painter_vit_large.pth \
     --attack_id ${ATTACK_ID} \
     --epsilon ${EPSILON} \
@@ -39,5 +39,5 @@ CUDA_VISIBLE_DEVICES=3 python eval/lol/painter_inference_lol_attack_gt_baseline.
 
 done 
 
-## bash eval/lol/eval_sammy_attack_with_clip_pgd_gt.sh
+## bash eval/lol/eval_sammy_attack_with_clip_pgd_gt_changeA.sh
 
